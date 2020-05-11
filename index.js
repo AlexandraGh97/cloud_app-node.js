d7b5a5de76c685ade1d24e0474bc1e55b830177e
AlexandraGh97 <alex.ioana1997@yahoo.com>
2020-05-11 13:30:36 +0000

    modified index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express")
 const app = express()
 app.use('/',express.static('frontend')) 
 
+app.get('/',(req,res) => {
+  res.sendFile('/home/ubuntu/environment/cloud_app-node.js/frontend/govData.html')
+    
+})
 
 app.use(express.json())
 app.use(express.urlencoded())
