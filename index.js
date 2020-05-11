66d6f9afa4a8446c27da8b6cab8764051fabc206
AlexandraGh97 <alex.ioana1997@yahoo.com>
2020-05-11 14:22:38 +0000

    modified backend

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express")
 
 const app = express()
-var path = require('path');
+
 app.use('/',express.static('frontend')) 
 
 app.get('/',(req,res) => {
   res.sendFile(__dirname + '/frontend' + '/govData.html')
-    
 })
+
 
 app.use(express.json())
 app.use(express.urlencoded())
